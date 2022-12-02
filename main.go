08c6b604e6822283adb7b3b40936be04d0ceffe4
TheWalkingFat1337 <84389366+TheWalkingFat1337@users.noreply.github.com>
2022-12-02 13:48:33 +0100

    main edit

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/TheWalkingFat1337/go-microservices/config"
 	"github.com/TheWalkingFat1337/go-microservices/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
+	config.Connect()
 	routes.UserRoute(router)
 	router.Run(":1234")
 }
