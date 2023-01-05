16e58f5a00d34d0cb0b803905e234472f65edb6b
xofoeN <rouvendergob@yahoo.com>
2023-01-05 11:07:24 +0100

    db.go angepass und mod / sum updated

--- config/db.go
+++ config/db.go
@@ -9,15 +9,8 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "root:1234@tcp(localhost:5432)/user"
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       dsn,   // data source name
-		DefaultStringSize:         256,   // default size for string fields
-		DisableDatetimePrecision:  true,  // disable datetime precision, which not supported before MySQL 5.6
-		DontSupportRenameIndex:    true,  // drop & create when rename index, rename index not supported before MySQL 5.7, MariaDB
-		DontSupportRenameColumn:   true,  // `change` when rename column, rename column not supported before MySQL 8, MariaDB
-		SkipInitializeWithVersion: false, // auto configure based on currently MySQL version
-	}), &gorm.Config{})
+	dsn := "root:1234@tcp(host.docker.internal:5432)/user"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
